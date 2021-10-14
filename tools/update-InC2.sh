a8a4ef715978809a8a23b5c949ea1cb6b3cc0690
Andy Gallo <andrew.gallo@ge.com>
2021-10-14 15:09:24 -0400

    tools update sh to use environment variable for SPACK_ROOT

--- tools/update-InC2.sh
+++ tools/update-InC2.sh
@@ -7,23 +7,29 @@ then
   cd ..
 fi
 
+if [[ -z "${SPACK_REPOS}" ]]; then
+  spackRepoDir="$SPACK_ROOT/var/spack/repos/hpmusic"
+else
+  spackRepoDir="${SPACK_REPOS}/hpmusic"
+fi
+
 # Make sure our repo exists
-if [ ! -d "$SPACK_ROOT/var/spack/repos/hpmusic" ]; then
-  spack repo create $SPACK_ROOT/var/spack/repos/hpmusic hpmusic
-  spack repo add $SPACK_ROOT/var/spack/repos/hpmusic
+if [ ! -d "$spackRepoDir" ]; then
+  spack repo create $spackRepoDir hpmusic
+  spack repo add $spackRepoDir
 fi
 
 # Now make sure our package exists
-if [ ! -d "$SPACK_ROOT/var/spack/repos/hpmusic/packages/inc" ]; then
-  spack create --skip-editor --repo $SPACK_ROOT/var/spack/repos/hpmusic --name inc
+if [ ! -d "$spackRepoDir/packages/inc" ]; then
+  spack create --skip-editor --repo $spackRepoDir --name inc
 fi
 
 # Move our tarball over
 rm -f inc.tar.gz
 tar -czf inc.tar.gz *
-mv inc.tar.gz $SPACK_ROOT/var/spack/repos/hpmusic/packages/inc/
+mv inc.tar.gz $spackRepoDir/packages/inc/
 
 # Make sure package.py is updated
-cp tools/package.py.TEMPLATE $SPACK_ROOT/var/spack/repos/hpmusic/packages/inc/package.py
-tarhash=`md5sum $SPACK_ROOT/var/spack/repos/hpmusic/packages/inc/inc.tar.gz | awk '{print $1}'`
-sed -i'.TEMPLATE' -e "s/<VERSION>/$tarhash/g" $SPACK_ROOT/var/spack/repos/hpmusic/packages/inc/package.py
+cp tools/package.py.TEMPLATE $spackRepoDir/packages/inc/package.py
+tarhash=`md5sum $spackRepoDir/packages/inc/inc.tar.gz | awk '{print $1}'`
+sed -i'.TEMPLATE' -e "s/<VERSION>/$tarhash/g" $spackRepoDir/packages/inc/package.py
